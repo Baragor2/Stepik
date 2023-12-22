a69ebb565406a41766a5365a3d081f838b8c39e1
Baragor2 <evgen3email@gmail.com>
2023-12-22 14:35:26 +0300

    Section_3,4 Step_2, Step_3, Step_4, Step_5, Step_6

---
+++ Stepik-Добрый, добрый Python/3 Постижение строк и списков/3,4 Спецсимволы и экранирование символов/Step_5.py
@@ -0,0 +1,9 @@
+'''
+Используя raw-строки, задайте строку, содержащую этот путь к файлу: C:\WINDOWS\System32\drivers\etc\hosts.
+Результат отобразите на экране.
+'''
+
+
+# put your python code here
+s1 = r'C:\WINDOWS\System32\drivers\etc\hosts'
+print(s1)
